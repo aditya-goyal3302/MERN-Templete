ddb38d04f95944cb323495918ca8d8189f0bbe87
Aditya Goyal <aditya.1155@zenmonk.tech>
2024-09-20 08:30:39 +0530

    Refactor custom components and add new ones

---
+++ client/src/components/custom/ItemCard/index.tsx
@@ -0,0 +1,25 @@
+import { Box, IconButton, Typography } from '@mui/material'
+import React from 'react'
+import styles from './ItemCard.module.css'
+
+type propData = {
+  
+}
+
+const ItemCard = (props:propData) => {
+  const {  } = props
+  return (
+    <Box className={styles.root}>
+          
+      <Box className={styles.imageWrapper}>
+        
+      </Box>
+      <Box className={styles.content}>
+        
+      </Box>
+
+    </Box>
+  )
+}
+
+export default ItemCard
